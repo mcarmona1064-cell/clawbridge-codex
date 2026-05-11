3eb52ca7675be11aae3852c240bb979759b8a4b3
Mark Carmona <markcarmona@Marks-MacBook-Air.local>
2026-05-10 21:33:58 -0700

    chore: remove portal

--- portal/api/src/middleware/cors.ts
+++
@@ -1,8 +0,0 @@
-import cors from 'cors';
-
-export const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:4000', 'http://localhost:3000'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-});
